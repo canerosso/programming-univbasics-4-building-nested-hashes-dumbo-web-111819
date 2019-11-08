054ad66579da1cf086c18b49ffc7d15bee85d0bf
Cane Rosso <astor@usa.com>
2019-11-08 20:08:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,9 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
+  # Remember implicit return! 
+  # the last expression in a method's implementation is the return value of the method itself.
+  #We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-[:railroads]=>{}
-	 monopoly = {railroads: "Dwayne The Rock Johnson"}
+bash_hash
 end
 #end
 

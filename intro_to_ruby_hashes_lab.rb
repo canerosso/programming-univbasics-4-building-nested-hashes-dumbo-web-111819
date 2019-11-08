2ab3fc603a3a444861080a7807d6bf1ca6ed1871
Cane Rosso <astor@usa.com>
2019-11-08 20:14:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,6 +53,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       names: {
+        :shortline_railroad => {}
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
